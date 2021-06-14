f2cfbb951dd7b80d3cd7f28d954904362b775dff
pej <ellen881217@gmail.com>
2021-06-14 18:10:07 +0800

    hw6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
-import MultiButton from './cgu_multiButton'
-import HelloCGU from './cgu_hello'
-import Inputs from './inputs'
+import ToDoApp from './ToDoApp'
 
 function App() {
   return(
     <div className="App">
-      <div>
-        {HelloCGU()}
-      </div>
-      <div>
-      { MultiButton(10) }
-      </div>
+    
+      {ToDoApp()}
     </div>
   );
 }
