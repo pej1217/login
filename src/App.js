8f6556fcbf3551b4531db19c1d56dbd0cb1369b4
pej <ellen881217@gmail.com>
2021-06-15 11:50:19 +0800

    datePicker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-//import logo from './logo.svg';
-import './App.css';
-import ToDoApp from './ToDoApp'
+import React,{useState} from 'react'
+import DatePicker from 'react-datepicker';
 
-function App() {
-  return(
-    <div className="App">
-    
-      {ToDoApp()}
+import "react-datepicker/dist/react-datepicker.css";
+
+
+function App(){
+  const [startDate, setStartDate] = useState(new Date());
+  const [isTW ,setisTW] = useState(0);
+
+  const newChange=(date)=>{
+    var start = date.getFullYear();
+    if (start>1911){
+        const newdate = date.setFullYear(start-1911);
+        setisTW(true);
+        return(setStartDate(newdate));
+    }
+    else{
+      return setStartDate(date)
+    }
+  }
+
+  return (
+    <div>
+    <DatePicker dateFormat={isTW?"民國yyy年/MM月/dd日":" "} selected={startDate}  onChange={date=>newChange(date)}/>
     </div>
   );
-}
-
+};
 export default App;
