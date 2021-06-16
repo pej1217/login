d23b332c9ca4a8157dbcbe82dfce6fc48fa37f82
pej <ellen881217@gmail.com>
2021-06-16 16:59:59 +0800

    login

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React,{useState} from 'react'
-import DatePicker from 'react-datepicker';
-
-import "react-datepicker/dist/react-datepicker.css";
+import React from 'react';
+import BasicTextFields from './login'
+import Button from '@material-ui/core/Button';
 
 
 function App(){
-  const [startDate, setStartDate] = useState(new Date());
-  const [isTW ,setisTW] = useState(0);
-
-  const newChange=(date)=>{
-    var start = date.getFullYear();
-    if (start>1911){
-        const newdate = date.setFullYear(start-1911);
-        setisTW(true);
-        return(setStartDate(newdate));
-    }
-    else{
-      return setStartDate(date)
-    }
-  }
-
-  return (
-    <div>
-    <DatePicker dateFormat={isTW?"民國yyy年/MM月/dd日":" "} selected={startDate}  onChange={date=>newChange(date)}/>
+  return(
+    <div class="container">  
+      <div>
+      {BasicTextFields()}
+      </div>
+      <div>
+      <Button ovariant="outlined" color="primary">login
+      </Button>  
+      </div>
     </div>
   );
-};
+}
 export default App;
